0fd7c54d029fcac45da2daee345e798d3b576754
Haroldo Oliveira de Almeida Leite <holiverleite@gmail.com>
2019-11-11 14:39:29 -0300

    - Implemented a cell to be shown in the listView on Home screen with expenses added.

--- ExpensesControlApp/MainPage.xaml.cs
+++ ExpensesControlApp/MainPage.xaml.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using Xamarin.Forms;
+using ExpensesControlApp.Model;
 
 namespace ExpensesControlApp
 {
@@ -18,23 +20,37 @@ namespace ExpensesControlApp
             InitializeComponent();
         }
 
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var expenses = new List<Expense>();
+
+            var e1 = new Expense(title:"Alimentação",description:"Praça de alimentação",amount:"R$200,00",date:new DateTime());
+            var e2 = new Expense(title: "Carro", description: "Mecanica", amount: "R$100,00", date: new DateTime());
+            var e3 = new Expense(title: "Transporte", description: "Onibus", amount: "R$20,00", date: new DateTime());
+            var e4 = new Expense(title: "Estadia", description: "Aluguel", amount: "R$130,00", date: new DateTime());
+            var e5 = new Expense(title: "Cinema", description: "Lazer", amount: "R$50,00", date: new DateTime());
+            var e6 = new Expense(title: "Alimentação", description: "Praça de alimentação", amount: "R$240,50", date: new DateTime());
+
+            expenses.Add(e1);
+            expenses.Add(e2);
+            expenses.Add(e3);
+            expenses.Add(e4);
+            expenses.Add(e5);
+            expenses.Add(e6);
+
+            listView.ItemsSource = expenses.ToList();
+        }
+
         async void createExpenseClicked(object sender, EventArgs e)
         {
-            //await Navigation.PushAsync(new NoteEntryPage
-            //{
-            //    BindingContext = new Note()
-            //});
+
         }
 
         async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
-            //if (e.SelectedItem != null)
-            //{
-            //    await Navigation.PushAsync(new NoteEntryPage
-            //    {
-            //        BindingContext = e.SelectedItem as Note
-            //    });
-            //}
+
         }
     }
 }
---
+++ ExpensesControlApp/Model/Expense.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ExpensesControlApp.Model
+{
+    public class Expense
+    {
+        public Expense(String title, String description, String amount, DateTime date)
+        {
+            this.expenseTitle = title;
+            this.expenseDescription = description;
+            this.expenseAmount = amount;
+            this.expenseDate = date;
+        }
+
+        public string expenseTitle { get; set; }
+        public string expenseDescription { get; set; }
+        public string expenseAmount { get; set; }
+        public DateTime expenseDate { get; set; }
+    }
+}
