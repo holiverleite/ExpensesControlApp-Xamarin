1ba555731ded9dc3bc919c2117259b4f6db574ca
Haroldo Oliveira de Almeida Leite <holiverleite@gmail.com>
2019-11-11 13:29:09 -0300

    - Created a main screen where the user can see each expense and the total spended.

---
+++ ExpensesControlApp.Android/obj/Debug/android/typemap.mj.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: eabd73e0a8a10e5bba7afa1ef5ef1ca1c49ca8e2 */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1420
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 372041
+	.include	"typemap.mj.inc"
---
+++ ExpensesControlApp/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ExpensesControlApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void createExpenseClicked(object sender, EventArgs e)
+        {
+            //await Navigation.PushAsync(new NoteEntryPage
+            //{
+            //    BindingContext = new Note()
+            //});
+        }
+
+        async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            //if (e.SelectedItem != null)
+            //{
+            //    await Navigation.PushAsync(new NoteEntryPage
+            //    {
+            //        BindingContext = e.SelectedItem as Note
+            //    });
+            //}
+        }
+    }
+}
